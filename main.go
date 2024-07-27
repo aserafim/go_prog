f530bd64385db7ca5691f085ff76394c063f885f
aserafim <correia.alefe@gmail.com>
2024-07-27 07:22:15 -0300

    Adicionado print do indice

--- main.go
+++ main.go
@@ -5,11 +5,24 @@ import (
 	"os"
 )
 
-func main() {
-	var s, sep string
-	for i := 1; i < len(os.Args); i++ {
-		s += sep + os.Args[i]
+//func main() {
+//	var s, sep string
+//	for i := 1; i < len(os.Args); i++ {
+//		s += sep + os.Args[i]
+//		sep = " "
+//	}
+//	fmt.Println(s)
+//}
+
+func main(){
+	s, sep := "", ""
+	for index, arg := range os.Args[0:]{ //ignora o índice
+		s += fmt.Sprintf("%d %s %s", index, sep, arg)
 		sep = " "
 	}
 	fmt.Println(s)
 }
+
+//func main(){
+//	fmt.Println(os.Args[0:])	
+//}
