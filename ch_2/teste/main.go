e87d908c746ade9d5bd36c8950a283b93eee0c54
aserafim <correia.alefe@gmail.com>
2024-08-05 16:24:57 -0300

    Etudando ponteiros

--- ch_2/teste/main.go
+++ ch_2/teste/main.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 var test int
 
@@ -9,5 +11,27 @@ func main() {
 	i := 0
 
 	fmt.Printf("Variável package level: %d , variável local: %d\n", test, i)
+
+	//Um ponteiro é o endereço da variável
+	//Em outras palavras é o local aonde
+	//o valor da variável é armazenado
+
+	//Declara e define o valor de x
+	x := 1
+
+	//p recebe o endereço de memória de x
+	p := &x
+
+	//imprie o endereço de memória
+	fmt.Print("\nEndereço de memória de p: ", p)
+
+	//imprime o valor armazenado em p
+	fmt.Println("\nValor armazenado em p: ", *p)
+
+	//Altera o valor armazenado em p
+	*p = 2
+
+	//imprime novamente
+	fmt.Println("Novo valor armazenado em p: ", x)
 
 }
